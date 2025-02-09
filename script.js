bde79dd64dab80244298ba8db2adaf8cb663d1ef
sandro <sandro.jashi@gmail.com>
2025-02-09 11:33:59 +0400

    this is a test file

---
+++ script.js
@@ -0,0 +1,77 @@
+// for(let i=0;i<5;i+=2){
+//   console.log(1);
+// }
+
+// let arr = ["apple", "orange", "banana"];
+// for (let i=0; i < arr.length; i++){
+// console.log("ინდექს"+i + ":" + arr[i]);
+
+// }
+// console.log(arr[1]);
+
+// let i=10;
+// while (i <5){
+// console.log(1);
+// i++;
+// }
+
+// do while
+
+// let i= 1;
+// do{
+// console.log(i);
+// i++;
+// } while (i <3);
+
+
+
+// ...
+
+// let arr1=["apple", "banana"];
+// let arr2 = ["orange", "carrot"];
+// let arr3=[...arr1, ...arr2];
+// console.log(arr3);
+
+// clone
+// let arr4=[1, 2, 3];
+// let copyArr = [ ...arr4];
+// console.log (copyArr);
+
+// let obj = {name: "jane", age: 25};
+// let cloneObj = {...obj};
+// console.log (cloneObj);
+
+// let obj1 = {city: "tbilisi"};
+// let mergedObj = {...obj, ...obj1};
+// console.log(mergedObj);
+
+// let, const, var 
+
+// function declaration
+
+// function sayHello(){
+//   let str= "hello world";
+//    console.log (str);
+//    if (str= "hello world"){
+//     let str1 = "hello";
+//     console.log("true");
+//    }
+// }
+// sayHello();
+// console.log(str)
+
+// let str2= "hello";
+// str2= "world";
+// str2= "greet";
+// console.log (str2);
+
+
+function count(){
+    console.log ("hello");
+}
+count();
+
+
+// regular function
+
+
